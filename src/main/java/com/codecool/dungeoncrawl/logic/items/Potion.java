63bfdd40717c05b1304f2f8886a20fac88e54870
Z20LET <trautmann.marton@outlook.com>
2023-01-11 13:03:45 +0100

    Add Potion

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Potion.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Potion.java
@@ -2,6 +2,7 @@ package com.codecool.dungeoncrawl.logic.items;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.actors.Actor;
+import com.codecool.dungeoncrawl.logic.actors.Player;
 
 public class Potion extends Actor {
 
@@ -10,5 +11,7 @@ public class Potion extends Actor {
     @Override
     public String getTileName() {
         return "potion";
+    }
+    public void Heal() {
     }
 }
